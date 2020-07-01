3a74f025bccc35053eb788fa28a61853255f69e1
weijin <1455236662@qq.com>
2020-07-01 22:39:33 +0800

    Revert "Revert "修改框架，加入顶部导航和搜索框""
    This reverts commit f76d20249001b98767cfe4079d4e0eea103d3ead.

---
+++ src/views/Predict.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <em>Welcome!</em>
+    <h1>这里是综报预测页面</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Predict"
+    }
+</script>
+
+<style scoped>
+
+</style>
